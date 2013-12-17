8f11a3392e6ff2383efcba2fee84f17e75e86dbe
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2013-12-17 08:41:47 -0200

    example

---
+++ beauty-nancyfx/Modules/HomeModule.cs
@@ -0,0 +1,10 @@
+﻿namespace beauty_nancyfx.Modules
+{
+    public class HomeModule : Nancy.NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
